1c304ba68a0e2727c508586caaf5096dc4f412b9
Dzela <dzelanovi@gmail.com>
2021-08-05 00:41:14 +0200

    ADD: Quick Footer Update

--- components/Footer.js
+++ components/Footer.js
@@ -3,25 +3,49 @@ function Footer() {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Ailbnb Luxe</p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          How airbnb works
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Newsroom
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Investors
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Airbnb Plus
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Ailbnb Luxe
+        </p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>Dzela knows</p>
-        <p>Friends</p>
-        <p>Miki milane</p>
-        <p>Go to Serbia bruh</p>
-        <p>Papa thank you for images 🔥</p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Dzela knows
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Friends
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Miki milane
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Go to Serbia bruh
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Papa thank you for images 🔥
+        </p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>Become a Host</p>
-        <p>Let Dzela Host you</p>
-        <p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Become a Host
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Let Dzela Host you
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
           Don't even think about <br />
           PAPA hosting you
         </p>
@@ -29,14 +53,22 @@ function Footer() {
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
-        <p>We don't abuse your info </p>
-        <p>It wasn't us</p>
-        <p>Still wrong</p>
-        <p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          We don't abuse your info{" "}
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          It wasn't us
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Still wrong
+        </p>
+        <p className="hover:underline transform transition duration-300 ease-out">
           Tutorial about Malicious Software and why <br />
           that isn't DzelaBnb
         </p>
-        <p>Still Love u papa 🐱‍👤</p>
+        <p className="hover:underline transform transition duration-300 ease-out">
+          Still Love u papa 🐱‍👤
+        </p>
       </div>
     </div>
   );
