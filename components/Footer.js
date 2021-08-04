90ed2798a2c72f5335e4b18c45f6124e37164bf1
Dzela <dzelanovi@gmail.com>
2021-08-05 00:43:45 +0200

    Footer Update 2.0

--- components/Footer.js
+++ components/Footer.js
@@ -3,49 +3,49 @@ function Footer() {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           How airbnb works
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Newsroom
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Investors
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Airbnb Plus
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Ailbnb Luxe
         </p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Dzela knows
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Friends
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Miki milane
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Go to Serbia bruh
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Papa thank you for images 🔥
         </p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Become a Host
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Let Dzela Host you
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Don't even think about <br />
           PAPA hosting you
         </p>
@@ -53,20 +53,20 @@ function Footer() {
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           We don't abuse your info{" "}
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           It wasn't us
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Still wrong
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Tutorial about Malicious Software and why <br />
           that isn't DzelaBnb
         </p>
-        <p className="hover:underline transform transition duration-300 ease-out">
+        <p className="cursor-pointer hover:underline transform transition duration-300 ease-out">
           Still Love u papa 🐱‍👤
         </p>
       </div>
